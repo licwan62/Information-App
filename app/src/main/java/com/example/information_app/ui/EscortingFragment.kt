97ef63a5d85cf8da52406387e9b2d2f823c0dd32
hzwlc <hzwlc6262@gmail.com>
2024-10-29 12:55:15 +1300

    make basic fragments navigate them

---
+++ app/src/main/java/com/example/information_app/ui/EscortingFragment.kt
@@ -0,0 +1,8 @@
+package com.example.information_app.ui
+
+import androidx.fragment.app.Fragment
+import com.example.information_app.R
+
+class EscortingFragment : Fragment(R.layout.fragment_escorting) {
+
+}
--- app/src/main/java/com/example/information_app/ui/HomeFragment.kt
+++ app/src/main/java/com/example/information_app/ui/HomeFragment.kt
@@ -2,14 +2,44 @@ package com.example.information_app.ui
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.information_app.R
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+    private lateinit var button_information: Button
+    private lateinit var button_escorting: Button
+    private lateinit var button_parents: Button
+    private lateinit var button_caregivers: Button
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        button_information = view.findViewById(R.id.button_information)
+        button_escorting = view.findViewById(R.id.button_escorting)
+        button_parents = view.findViewById(R.id.button_parents)
+        button_caregivers = view.findViewById(R.id.button_caregivers)
 
+        button_information.setOnClickListener {
+            var action = HomeFragmentDirections.actionHomeFragmentToInformationFragment()
+            findNavController().navigate(action)
+        }
+
+        button_escorting.setOnClickListener {
+            var action = HomeFragmentDirections.actionHomeFragmentToEscortingFragment()
+            findNavController().navigate(action)
+        }
+
+        button_parents.setOnClickListener {
+            var action = HomeFragmentDirections.actionHomeFragmentToParentFragment()
+            findNavController().navigate(action)
+        }
+
+        button_caregivers.setOnClickListener {
+            var action = HomeFragmentDirections.actionHomeFragmentToCaregiverFragment()
+            findNavController().navigate(action)
+        }
 
     }
 }
