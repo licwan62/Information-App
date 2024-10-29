de3e738e7b30c5267924eb707f182a496f54983b
hzwlc <hzwlc6262@gmail.com>
2024-10-29 22:13:00 +1300

    start multi-language

--- app/src/main/java/com/example/information_app/ui/HomeFragment.kt
+++ app/src/main/java/com/example/information_app/ui/HomeFragment.kt
@@ -1,25 +1,55 @@
 package com.example.information_app.ui
 
+import android.content.res.Configuration
+import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.information_app.R
+import com.google.android.material.snackbar.Snackbar
+import java.util.Locale
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+    private lateinit var button_language: Button
     private lateinit var button_information: Button
     private lateinit var button_escorting: Button
     private lateinit var button_parents: Button
     private lateinit var button_caregivers: Button
+    private val languages = arrayOf("en", "rar")
+    private var current_language = 0
+
+
+    fun setLocal(languageCode: String) {
+        val locale = Locale(languageCode)
+
+        val config = Configuration(resources.configuration)
+            .apply {
+                setLocale(locale)
+            }
+
+        button_language.text = languageCode
+
+        val msg = "language changed to " + languageCode
+        Snackbar.make(requireView(), msg, Snackbar.LENGTH_LONG).show()
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        button_language = view.findViewById(R.id.button_language)
         button_information = view.findViewById(R.id.button_information)
         button_escorting = view.findViewById(R.id.button_escorting)
         button_parents = view.findViewById(R.id.button_parents)
         button_caregivers = view.findViewById(R.id.button_caregivers)
+
+        button_language.setOnClickListener {
+            current_language = (current_language + 1) % 2
+            val language_code = languages[current_language]
+            setLocal(language_code)
+        }
 
         button_information.setOnClickListener {
             var action = HomeFragmentDirections.actionHomeFragmentToInformationFragment()
@@ -40,6 +70,6 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
             var action = HomeFragmentDirections.actionHomeFragmentToCaregiverFragment()
             findNavController().navigate(action)
         }
-
     }
+
 }
--- app/src/main/java/com/example/information_app/ui/MainActivity.kt
+++ app/src/main/java/com/example/information_app/ui/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.example.information_app.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.information_app.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var nav_controller: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // set up action bar
+        val nav_host_fragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        nav_controller = nav_host_fragment.findNavController()
+        setupActionBarWithNavController(nav_controller)
+
+    }
+
+    // set up navigate back button on action bar
+    override fun onSupportNavigateUp(): Boolean {
+        return super.onSupportNavigateUp() || nav_controller.navigateUp()
     }
 }
