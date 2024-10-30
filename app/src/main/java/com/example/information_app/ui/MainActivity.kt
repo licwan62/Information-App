42d26b48e5a0ab48d71f9ba81f4f7f45148294a2
hzwlc <hzwlc6262@gmail.com>
2024-10-30 17:08:00 +1300

    better toolbar

--- app/src/main/java/com/example/information_app/ui/MainActivity.kt
+++ app/src/main/java/com/example/information_app/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.information_app.ui
 
 import android.os.Bundle
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -9,22 +10,27 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.information_app.R
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var nav_controller: NavController
+    private lateinit var navController: NavController
+    private lateinit var toolbar: androidx.appcompat.widget.Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // set up action bar
-        val nav_host_fragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
-        nav_controller = nav_host_fragment.findNavController()
-        setupActionBarWithNavController(nav_controller)
+        toolbar = findViewById(R.id.toolbar)
+
+        // get NavController
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        // set up toolbar
+        setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navController)
 
     }
 
     // set up navigate back button on action bar
     override fun onSupportNavigateUp(): Boolean {
-        return super.onSupportNavigateUp() || nav_controller.navigateUp()
+        return super.onSupportNavigateUp() || navController.navigateUp()
     }
 }
