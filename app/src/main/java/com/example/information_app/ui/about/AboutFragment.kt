ee71b8331d34df9d45d6a296e88a5097f0112911
hzwlc <hzwlc6262@gmail.com>
2024-11-09 01:32:35 +1300

    english paragraphs
    Former-commit-id: cea8ed0f8ad43e941959249d20e2843b9b4f26f4
    Former-commit-id: 1bee8bb6cf11abf24c45b29507ae0257cd4a4921

--- app/src/main/java/com/example/information_app/ui/about/AboutFragment.kt
+++ app/src/main/java/com/example/information_app/ui/about/AboutFragment.kt
@@ -1,27 +1,34 @@
-package com.example.information_app.ui.infomation
+package com.example.information_app.ui.about
 
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import android.view.View
 import androidx.core.text.HtmlCompat
 import androidx.fragment.app.Fragment
 import com.example.information_app.R
-import com.example.information_app.databinding.FragmentInformationBinding
+import com.example.information_app.databinding.FragmentAboutBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class InformationFragment : Fragment(R.layout.fragment_information) {
-    private lateinit var binding: FragmentInformationBinding
+class AboutFragment : Fragment(R.layout.fragment_about) {
+    private lateinit var binding: FragmentAboutBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding = FragmentInformationBinding.bind(view)
+        binding = FragmentAboutBinding.bind(view)
         binding.apply {
             textViewParagraph.text =
                 HtmlCompat.fromHtml(
                     getString(R.string.about_text),
                     HtmlCompat.FROM_HTML_MODE_LEGACY
                 )
+
+            // enable link click event to its href
+            textViewParagraph.movementMethod =
+                LinkMovementMethod.getInstance()
+
+//            webView.loadDataWithBaseURL(null, htmlContent, "text/html", "UTF-8", null)
         }
     }
 }
