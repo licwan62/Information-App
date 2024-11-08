d619e7866d1f91402fd862005677cfd6f4787e6b
hzwlc <hzwlc6262@gmail.com>
2024-11-08 22:24:52 +1300

    coop maori language

--- app/src/main/java/com/example/information_app/data/Question.kt
+++ app/src/main/java/com/example/information_app/data/Question.kt
@@ -1,6 +1,7 @@
 package com.example.information_app.data
 
 import android.os.Parcelable
+import androidx.annotation.StringRes
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
@@ -8,12 +9,12 @@ import kotlinx.android.parcel.Parcelize
 @Entity(tableName = "question_table")
 @Parcelize
 data class Question(
-    val text: String,
+    @StringRes val textRes: Int, // string resource reference by id
     val correctAnswer: Boolean, // right answer to the question
-    val explanation: String = "",
+    @StringRes val explanationRes: Int,
     val userAnswer: Boolean = false, // user input answer
     @PrimaryKey(autoGenerate = true) val id: Int = 0
 ) : Parcelable {
-    var isAnswerCorrect: Boolean = false
+    val isAnswerCorrect: Boolean
         get() = correctAnswer == userAnswer
 }
